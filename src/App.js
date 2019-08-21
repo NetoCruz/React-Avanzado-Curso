723a7fca7b0752430862302ee383985b6b7b0036
unknown <galaxy.net0710@gmail.com>
2019-08-21 15:45:18 -0500

    rutas protegidas usuarios no registrados

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisterdUser'
 
 import { Router } from '@reach/router'
+import Context from './Context'
 
 export const App = () => {
   return (
@@ -16,7 +20,24 @@ export const App = () => {
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+
       </Router>
+      <Context.Consumer>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+
+        }
+
+      </Context.Consumer>
       <NavBar />
     </div>
   )
